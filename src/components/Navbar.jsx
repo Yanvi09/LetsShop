fb9b30330a50fea9d4fff550ef6e9edf1e2fe49a
Anvi <20yanvi05@gmial.com>
2025-08-20 12:49:03 +0530

    Added Navbar styling and functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,12 +17,86 @@ top: 0;
 z-index: 10;
 color: white;
 `;
-const NavbarContainer = styled.div``;
-const NavLogo = styled.div``;
-const Logo = styled.img``;
-const NavItems = styled.div``;
-const NavButtonContainer= styled.div``;
-const Navlink= styled(Navlink)``;
+const NavbarContainer = styled.div`
+width: 100%;
+max-width: 1400px;
+padding: 0 24px;
+display: flex;
+gap: 14px;
+align-items: center;
+justify-content: space-between;
+font-size: 1rem;
+
+
+`;
+const NavLogo = styled.div`
+width: 100%
+display: flex;
+align-items: center;
+padding: 0 6px;
+font-weight:500;
+font-size: 18px;
+text-decoration: none;
+color: inherit;
+
+
+`;
+const Logo = styled.img`
+height: 34px;
+
+
+`;
+const NavItems = styled.ul`
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 32px;
+padding: 0 6px;
+list-style: none;
+@media screen and (max-width: 768px){
+display: none;
+
+}
+
+`;
+
+
+const Navlink= styled(Navlink)`
+display: flex;
+align-items: center;
+color: ${({ theme }) => theme.text_primary};
+font-weight: 500;
+cursor: pointer;
+transition: all ls slide-in;
+text-decoration: none;
+&:hover {
+color: ${({ theme}) => theme.primary};
+
+}
+
+&:active {
+color: ${({ theme}) => theme.primary};
+border-bottom: 1.8px solid ${({ theme }) => theme.primary};
+
+}
+
+`;
+
+const ButtonContainer= styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: flex-end;
+gap: 28px;
+align-items: center;
+padding: 0 6px;
+color: ${({ theme}) => theme.primary};
+@media screen and (max-width: 768px) {
+display: none;
+}
+
+`;
 
  
 
