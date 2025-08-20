2524ee9aa295b4b7f621f7427d140f438dc10619
Anvi <20yanvi05@gmial.com>
2025-08-20 23:19:04 +0530

    Build Authentication added SignIn and SignUp Functionality

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
+import React from "react";
 import styled, { ThemeProvider } from "styled-components";
-import {lightTheme} from "./utils/Themes";
+import { lightTheme } from "./utils/Themes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 
 const Container = styled.div`
-   width: 100%;
-   height: 100vh;
-   dispaly: flex;
-   flex-direction: column;
-   background: ${({theme }) => theme.bg};
-   color: ${({ theme }) => theme.text_primary};
-
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
 `;
 
-
 function App() {
-  return <ThemeProvider theme={lightTheme}>
-   <BrowserRouter>
-   <Container>
-    <Navbar>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-    
-    
-    </Routes></Navbar></Container>
-   </BrowserRouter>
-
-  </ThemeProvider>
-  
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
